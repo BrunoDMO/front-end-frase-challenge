3ad76eb94b091072a77d470aa9fd13f1e080ac8e
BrunoDMO <brunonaw41@gmail.com>
2024-03-05 16:34:47 -0300

    front remodelated and connected and mobile view

--- scripts/index.js
+++ scripts/index.js
@@ -6,33 +6,6 @@ const elementos = {
     lancamentos: document.querySelector('[data-name="lancamentos"]'),
     series: document.querySelector('[data-name="series"]')
 };
-
-// Função para criar a lista de filmes
-
-// Função para criar a lista de filmes
-function criarListaFilmes(elemento, dados) {
-    // Verifique se há um elemento <ul> dentro da seção
-    const ulExistente = elemento.querySelector('ul');
-
-    // Se um elemento <ul> já existe dentro da seção, remova-o
-    if (ulExistente) {
-        elemento.removeChild(ulExistente);
-    }
-
-    const ul = document.createElement('ul');
-    ul.className = 'lista';
-    const listaHTML = dados.map((filme) => `
-        <li>
-            <a href="/detalhes.html?id=${filme.id}">
-                <img src="${filme.poster}" alt="${filme.titulo}">
-            </a>
-        </li>
-    `).join('');
-
-    ul.innerHTML = listaHTML;
-    elemento.appendChild(ul);
-}
-
 // Função genérica para tratamento de erros
 function lidarComErro(mensagemErro) {
     console.error(mensagemErro);
@@ -101,19 +74,23 @@ function criarFrase(elemento, dados) {
 
     const ul = document.createElement('ul');
     const listaHTML = dados.map((frase) => ` 
-    <p class="descricao"> Serie: ${frase.titulo} </p>
-    <li>                
-        <a>
-            <img src="${frase.poster}" alt="frase poster">
-        </a>
-    </li>
+    <div class="frase_text">
+        <p class="descricao titulo"> ${frase[0].titulo} </p>   
+        <p class="descricao frase"> "${frase[0].frase} </p>
+        <p class="descricao personagem"> By: ${frase[0].personagem}</p>
+    </div>  
+      
+    <a> 
+        <img class="descricao poster" src="${frase[0].poster}" alt="frase poster">
+        <div class="cabecalho__logo" id="box-1">
+            <img src="./img/logo.png" alt="Logo ScreenMatch">
+            <h2 class="titulo">
+                Frase do dia!
+            </h2>
+        </div>
+    </a>
+    
     `);
-    // const paragrafo = document.createElement('p');
-    // const linha = document.createElement('br');
-    // paragrafo.textContent = `Temporada ${temporada}`;
-    // elemento.appendChild(paragrafo);
-    // elemento.appendChild(linha);
-    // elemento.appendChild(ul);
 
     ul.innerHTML = listaHTML;
     elemento.appendChild(ul);
